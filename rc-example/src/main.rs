eec8c882f0fb2a04a4583c0dde048233bf79635f
Takashi Kasai <takassh23@gmail.com>
2024-01-09 17:59:39 +0900

    rc-example

--- rc-example/src/main.rs
+++ rc-example/src/main.rs
@@ -1,12 +1,48 @@
+#[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
+use std::rc::Rc;
+
 use crate::List::{Cons, Nil};
 
-fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    let c = Cons(4, Box::new(a));
+impl Drop for List {
+    fn drop(&mut self) {
+        println!("dropped : {:?}", self);
+    }
 }
+
+fn main() {
+    // You don't need to make let for Nil and Cons(10, Rc::new(Nil))
+    // It's automatically freed when every owner is dropped
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+}
+
+
+//// Similar behavior but it's different. See let n and let i
+// #[derive(Debug)]
+// enum List<'a> {
+//     Cons(i32, Box<&'a List<'a>>),
+//     Nil,
+// }
+
+// use crate::List::{Cons, Nil};
+
+// impl<'a> Drop for List<'a> {
+//     fn drop(&mut self) {
+//         println!("dropped : {:?}", self);
+//     }
+// }
+
+// fn main() {
+//     // You need to declare n and i
+//     let n = Nil;
+//     let i = Cons(10, Box::new(&n));
+//     let a = Cons(5, Box::new(&i));
+//     let b = Cons(3, Box::new(&a));
+//     let c = Cons(4, Box::new(&a));
+// }
